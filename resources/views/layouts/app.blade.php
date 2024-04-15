024834b378fd851267ab974fad082a33730cdf3a
mikitosina1 <mikitosina@mail.ru>
2024-04-15 22:29:01 +0200

    Revert "feat: move css from welcome.blade.php to app.css"
    This reverts commit e34d2124b21417f36ae2a839ab4d09784fb02bfc.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'mikitosina\'s blog' ) }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -29,7 +29,7 @@
 
             <!-- Page Content -->
             <main>
-                @yield('content')
+                {{ $slot }}
             </main>
         </div>
     </body>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.app')
-@section('content')
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -29,4 +27,3 @@
         </div>
     </div>
 </x-app-layout>
-@endsection
