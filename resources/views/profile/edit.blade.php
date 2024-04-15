e34d2124b21417f36ae2a839ab4d09784fb02bfc
mikitosina1 <mikitosina@mail.ru>
2024-04-15 22:18:56 +0200

    feat: move css from welcome.blade.php to app.css * app.blade.php now skeleton for everyone

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.app')
+@section('content')
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -27,3 +29,4 @@
         </div>
     </div>
 </x-app-layout>
+@endsection
