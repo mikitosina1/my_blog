ad59d4356f993c3a6297c48ffa98ce0e5a5b2900
mikitosina1 <mikitosina@mail.ru>
2024-05-20 00:37:07 +0200

    fix: changes for frontend part

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'mikitosina\'s blog') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -29,6 +29,10 @@
 
             <!-- Page Content -->
             <main>
+                <x-auth-main-plank class="">
+                </x-auth-main-plank>
+                <x-language class="">
+                </x-language>
                 {{ $slot }}
             </main>
         </div>
