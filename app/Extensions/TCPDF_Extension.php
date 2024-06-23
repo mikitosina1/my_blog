0433d58dfa5590ef16746670bbf6e29e034403fa
mikitosina1 <mikitosina@mail.ru>
2024-06-23 23:32:39 +0200

    feat: added extension for tcpdf class for custom elements

---
+++ app/Extensions/TCPDF_Extension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Extensions;
+
+use TCPDF;
+
+class TCPDF_Extension extends TCPDF
+{
+    public function Footer(): void
+    {
+        // Position at 15 mm from bottom
+        $this->SetY(-15);
+        // Set font
+        $this->SetFont('helvetica', 'I', 8);
+        // Page number
+        $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+    }
+}
