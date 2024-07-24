aff6f7457c05c703cb51e51cf6369b8aefcc6ead
mikitosina1 <mikitosina@mail.ru>
2024-07-24 21:01:22 +0200

    fix: removed adding btn-back, in cause that it's exists in another view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,5 @@
 @vite(['resources/css/dashboard.css','resources/js/pages/dashboard.js'])
 <x-app-layout>
-	<x-btn-back/>
 	<div class="dashboard_carpet">
 		@include('dashboard_blocks.module_block')
 	</div>
