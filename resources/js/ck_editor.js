d0cfef47c4b7072e569d37539f58f8322849f8d8
mikitosina1 <mikitosina@mail.ru>
2024-07-28 23:57:29 +0200

    feat: playing with ckeditor

--- resources/js/ck_editor.js
+++ resources/js/ck_editor.js
@@ -11,13 +11,15 @@ async function initializeEditor() {
 		const { default: ClassicEditor } = await import('@ckeditor/ckeditor5-build-classic');
 
 		const editor = await ClassicEditor.create(document.querySelector('#editor'), {
-			// Пример настроек конфигурации CKEditor
-			toolbar: ['heading', '|', 'bold', 'italic', 'link'],
+			toolbar: ['heading', '|', 'bold', 'italic', 'strikethrough', 'subscript', 'superscript', '|', 'link', 'codeBlock', 'imageUpload'],
 			heading: {
 				options: [
 					{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
 					{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
 					{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+					{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+					{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+					{ model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
 				]
 			}
 		});
--- resources/views/articles/test.blade.php
+++ resources/views/articles/test.blade.php
@@ -2,9 +2,8 @@
 <div class="content-block p-4 sm:p-8 dark:bg-gray-800 shadow sm:rounded-lg mt-8">
 	<form method="POST" action="posts/save">
 		@csrf
-		<textarea name="content" id="editor">
-			@lang('basic.your_text')
-		</textarea>
+		<!-- Powered by CKEditor -->
+		<textarea name="content" id="editor" placeholder="@lang('basic.your_text')"></textarea>
 		<button type="submit" class="text-gray-800 personalized">@lang('basic.save')</button>
 	</form>
 </div>
