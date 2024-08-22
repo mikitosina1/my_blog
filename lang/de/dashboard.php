fe2a2b7e67ba0860e552068bc20cdb68f30a52e0
mikitosina1 <mikitosina@mail.ru>
2024-08-22 13:30:01 +0200

    feat: lang files moved to module

--- lang/de/dashboard.php
+++ lang/de/dashboard.php
@@ -3,10 +3,8 @@
 return [
 	'dashboard' => 'Dashboard',
 	'alt' => 'Dein Profilbild',
-	'subm' => 'Aktualisieren',
 	'success' => 'Erfolgreich aktualisiert',
 	'photo_button' => 'Neues Profilfoto',
 	'picture_needs' => 'Das Foto sollte 2 MB nicht überschreiten und eine der Erweiterungen haben: jpeg,png,jpg,gif. Nachdem Sie ein Foto ausgewählt haben, klicken Sie auf die Schaltfläche „Aktualisieren“.',
     'profile' => 'Profil',
-	'manage_modules' => 'Modulverwaltung',
 ];
--- lang/en/dashboard.php
+++ lang/en/dashboard.php
@@ -3,10 +3,8 @@
 return [
 	'dashboard' => 'Dashboard',
 	'alt' => 'Your profile photo',
-	'subm' => 'Update',
 	'success' => 'Updated successfully',
 	'photo_button' => 'New profile photo',
 	'picture_needs' => 'The photo should not exceed 2MB and be one of the extensions: jpeg,png,jpg,gif. After selecting a photo, click the \'Update\' button.',
     'profile' => 'Profile',
-	'manage_modules' => 'Module management',
 ];
