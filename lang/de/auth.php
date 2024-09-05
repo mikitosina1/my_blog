8cf5425fd5758529b3201a7e3346f0cfff37e2c8
mikitosina1 <mikitosina@mail.ru>
2024-09-05 21:33:46 +0200

    feat: added languages to profile settings

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -18,4 +18,26 @@ return [
 	'throttle' => 'Zu viele Anmeldeversuche. Bitte versuchen Sie es in :5 Minuten erneut.',
 	'registered' => 'Sie sind erfolgreich registriert! Jetzt können Sie die Erweiterungsfunktion nutzen. ^^',
 	'forgot_password_btn' => 'Haben Sie Ihr Passwort vergessen?',
+	'profile_information' => 'Profil Informationen',
+	'profile_information_explanation' => 'Aktualisieren Sie die Profilinformationen und die E-Mail-Adresse Ihres Kontos.',
+	'name' => 'Vorname',
+	'surname' => 'Nachname',
+
+	'email_unverified' => 'Ihre E-Mail-Adresse ist nicht verifiziert.',
+	'email_verification_resend' => 'Klicken Sie hier, um die Bestätigungs-E-Mail erneut zu senden.',
+	'email_verification_sent' => 'Es wurde ein neuer Verifizierungslink an Ihre E-Mail-Adresse gesendet.',
+	'save' => 'Speichern',
+
+	'password_label' => 'Passwort',
+	'update_password' => 'Passwort aktualisieren',
+	'password_insurance' => 'Stellen Sie sicher, dass Ihr Konto ein langes, zufälliges Passwort verwendet, um sicher zu sein.',
+	'current_password' => 'Aktuelles Passwort',
+	'new_password' => 'Neue Passwort',
+	'confirm_password' => 'Bestätigen Sie Ihr Passwort',
+
+	'delete_account' => 'Konto löschen',
+	'delete_account_warning_message' => 'Nach der Löschung Ihres Kontos werden alle Ressourcen und Daten dauerhaft gelöscht. Bevor Sie Ihr Konto löschen, laden Sie bitte alle Daten oder Informationen herunter, die Sie aufbewahren möchten.',
+	'delete_account_success' => 'Ihr Konto wurde gelöscht.',
+	'delete_account_warning_message2' => 'Sind Sie sicher, dass Sie Ihr Konto löschen möchten?',
+	'сancel' => 'Abbrechen',
 ];
--- lang/en/auth.php
+++ lang/en/auth.php
@@ -18,4 +18,26 @@ return [
 	'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 	'registered' => 'You\'re registered successfully! Now you can use expand functionality. ^^',
 	'forgot_password_btn' => 'Forgot your password?',
+	'profile_information' => 'Profile information',
+	'profile_information_explanation' => 'Update your account\'s profile information and email address.',
+	'name' => 'Name',
+	'surname' => 'Surname',
+
+	'email_unverified' => 'Your email address is unverified.',
+	'email_verification_resend' => 'Your email address is unverified.',
+	'email_verification_sent' => 'A new verification link has been sent to your email address.',
+	'save' => 'Save',
+
+	'password_label' => 'Password',
+	'update_password' => 'Update Password',
+	'password_insurance' => 'Ensure your account is using a long, random password to stay secure.',
+	'current_password' => 'Current Password',
+	'new_password' => 'New Password',
+	'confirm_password' => 'Confirm Password',
+
+	'delete_account' => 'Delete Account',
+	'delete_account_warning_message' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
+	'delete_account_success' => 'Your account has been deleted.',
+	'delete_account_warning_message2' => 'Are you sure you want to delete your account?',
+	'сancel' => 'Cancel',
 ];
