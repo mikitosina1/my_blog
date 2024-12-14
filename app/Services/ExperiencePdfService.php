4ad5b062e05f2cb46173790ad81a3a45c8b0baa1
mikitosina1 <mikitosina@mail.ru>
2024-12-14 17:26:03 +0100

    fix: changed name of class because of collision

--- app/Services/ExperiencePdfService.php
+++ app/Services/ExperiencePdfService.php
@@ -3,26 +3,24 @@
 namespace App\Services;
 
 use App\Contracts\PdfServiceInterface;
-use App\Extensions\TCPDF_ExtensionResume;
+use App\Extensions\TCPDF_Extension_Resume;
 use Illuminate\Http\Request;
 
 class ExperiencePdfService implements PdfServiceInterface
 {
-    /** @var TCPDF_ExtensionResume */
-    protected TCPDF_ExtensionResume $tcpdf;
+    /** @var TCPDF_Extension_Resume */
+    protected TCPDF_Extension_Resume $tcpdf;
 
     public function __construct()
     {
-        $this->tcpdf = new TCPDF_ExtensionResume();
+        $this->tcpdf = new TCPDF_Extension_Resume();
     }
     public function generatePdf(Request $request): string
     {
-        // Логика создания описания опыта работы с использованием TCPDF
         $this->tcpdf->AddPage();
         $this->tcpdf->SetFont('times', 'B', 16);
         $this->tcpdf->Cell(40, 10, $request->get('type'));
 
-        // Возвращаем содержимое PDF-документа в виде строки
         return $this->tcpdf->Output($request->get('type').'.pdf', 'I');//I to S
     }
 }
