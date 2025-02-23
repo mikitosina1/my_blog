d13dd257c7130ef02ddb18cdba8a1de5b1100b2c
mikitosina1 <mikitosina@mail.ru>
2025-02-24 00:42:16 +0100

    fix: migration title role to roles table connection

---
+++ database/migrations/2024_10_12_000001_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('users', function (Blueprint $table) {
+			$table->id();
+			$table->string('name');
+			$table->string('lastname')->default("");
+			$table->string('email')->unique();
+			$table->timestamp('email_verified_at')->nullable();
+			$table->string('password');
+			$table->string('profile_photo')->default("");
+			$table->string('bio')->default("");
+			$table->unsignedBigInteger('role')->default(11);
+			$table->ipAddress()->default('');
+			$table->rememberToken();
+			$table->timestamps();
+
+			$table->foreign('role')->references('id')->on('roles');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('users');
+	}
+};
