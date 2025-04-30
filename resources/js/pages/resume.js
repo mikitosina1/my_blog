171616f5360a360040b5421c47c41bf6678f6b3e
mikitosina1 <mikitosina@mail.ru>
2025-05-01 00:23:35 +0200

    feat: continue logic of creating chat flat

--- resources/js/pages/resume.js
+++ resources/js/pages/resume.js
@@ -65,10 +65,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	const createCloseButton = (block, counterType) => {
 		let closeButton = $('<div class="close"></div>').html(`
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
-            </svg>
-        `);
+			<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+				<path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+			</svg>
+		`);
 		closeButton.click(function () {
 			if (counterType === 'experience') additionalExpCounter--;
 			if (counterType === 'studying') additionalStudCounter--;
