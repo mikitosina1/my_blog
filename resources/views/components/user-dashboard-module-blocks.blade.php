3146047289d9581f408db612d937a2d43c438ae9
mikitosina1 <mikitosina27@gmail.com>
2025-08-11 17:39:11 +0200

    feat: normalized the basic dashboard view to add widgets as modules in the structure

---
+++ resources/views/components/user-dashboard-module-blocks.blade.php
@@ -0,0 +1,3 @@
+<div class="user_dashboard_carpet">
+	{{ $slot }}
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,24 @@
 @vite(['resources/css/dashboard.css','resources/js/pages/dashboard.js'])
+@php
+	use Nwidart\Modules\Facades\Module;
+	$modules = Module::allEnabled();
+@endphp
+
 <x-app-layout>
-	<x-dashboard-blocks>
-		{{-- here will be content, but later --}}
-	</x-dashboard-blocks>
+	<x-user-dashboard-module-blocks>
+		<div class="cloud">
+			<div class="cloud-top">
+				@lang('dashboard.dashboard')
+			</div>
+			<div class="cloud-widgets">
+				@foreach ($modules as $module)
+					@php
+						$viewName = strtolower($module->getName()) . '::dashboard-block';
+					@endphp
+					@includeIf($viewName)
+				@endforeach
+			</div>
+		</div>
+	</x-user-dashboard-module-blocks>
 </x-app-layout>
+
