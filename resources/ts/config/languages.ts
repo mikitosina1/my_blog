d2086325d7fbf940777bdbfe7d07828e68efecfb
mikitosina1 <mikitosina27@gmail.com>
2026-08-08 02:20:43 +0200

    feat: added lucide react, rebuild language interface

---
+++ resources/ts/config/languages.ts
@@ -0,0 +1,19 @@
+export interface Language {
+    code: string;
+    name: string;
+}
+
+export const languages: Language[] = [
+    {
+        code: 'en',
+        name: 'English',
+    },
+    {
+        code: 'ru',
+        name: 'Русский',
+    },
+    {
+        code: 'de',
+        name: 'Deutsch',
+    },
+];
