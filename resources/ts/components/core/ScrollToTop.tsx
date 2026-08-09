f338b3c0afaa173bf94ed62256344b08317fc227
mikitosina1 <mikitosina27@gmail.com>
2026-08-09 22:26:23 +0200

    fix: scrolling next page for SPA

---
+++ resources/ts/components/core/ScrollToTop.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const content = document.querySelector(
+            '.app-layout__content'
+        );
+
+        if (content) {
+            content.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'instant',
+            });
+        }
+    }, [pathname]);
+
+    return null;
+}
