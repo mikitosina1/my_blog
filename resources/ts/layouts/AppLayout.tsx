ca7610613572a3ffe16e3c439ca9064e691d321a
mikitosina1 <mikitosina27@gmail.com>
2026-08-09 23:22:15 +0200

    fix: scroll fix

--- resources/ts/layouts/AppLayout.tsx
+++ resources/ts/layouts/AppLayout.tsx
@@ -6,9 +6,12 @@ import { AppSidebar } from '@/components/core/sidebar';
 import { AppTopbar } from '@/components/core/topbar';
 import { FloatingLayer } from '@/components/core/floating';
 
+import ScrollToTop from '@/components/core/ScrollToTop';
+
 export default function AppLayout() {
     return (
         <div className="app-layout">
+            <ScrollToTop />
 
             <AppSidebar/>
 
