28ef5b3f216108b80e64b5efc9b6396aa148c8b0
mikitosina1 <mikitosina27@gmail.com>
2026-08-09 23:21:52 +0200

    fix: remove empty spaces

--- resources/ts/types/Menu.ts
+++ resources/ts/types/Menu.ts
@@ -1,17 +1,10 @@
 export interface MenuItem {
     id: string;
-
     title: string;
-
     route: string;
-
     section: string;
-
     order: number;
-
     active?: boolean;
-
     icon?: string;
-
     module?: string;
 }
