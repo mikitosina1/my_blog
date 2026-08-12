4c32dd77e587abd275cbe89d29e99071877387e7
mikitosina1 <mikitosina27@gmail.com>
2026-08-12 20:44:43 +0200

    feat: created basic for Module Manager

--- resources/ts/app/generated/modules.ts
+++ resources/ts/app/generated/modules.ts
@@ -2,9 +2,11 @@
 // DO NOT EDIT MANUALLY.
 
 import module0 from '../../../../Modules/MigraineDiary/resources/ts/index';
+import module1 from '../../../../Modules/ModuleManager/resources/ts/index';
 
 const modules = [
-    module0
+    module0,
+    module1
 ];
 
 export default modules;
