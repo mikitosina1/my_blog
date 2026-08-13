ccc0293a1f1cfcfcccdc8d0139ab3dca5d29c507
mikitosina1 <mikitosina27@gmail.com>
2026-08-13 20:29:44 +0200

    fix: throw tags

--- app/Http/Controllers/Api/V1/AppBootstrapController.php
+++ app/Http/Controllers/Api/V1/AppBootstrapController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Services\AppBootstrapService;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\JsonResponse;
+use JsonException;
 
 readonly class AppBootstrapController
 {
@@ -11,6 +13,10 @@ readonly class AppBootstrapController
         private AppBootstrapService $bootstrap,
     ) {}
 
+    /**
+     * @throws FileNotFoundException
+     * @throws JsonException
+     */
     public function __invoke(): JsonResponse
     {
         return response()->json(
