713f54c1ed8c751ae151bc041f210c8b616ce921
mikitosina1 <mikitosina27@gmail.com>
2026-08-17 00:11:02 +0200

    fix: tailwind returned

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,27 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-		'./storage/framework/views/*.php',
-		'./resources/views/**/*.blade.php',
-	],
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/ts/**/*.{js,ts,jsx,tsx}',
+        './Modules/**/resources/ts/**/*.{js,ts,jsx,tsx}',
+    ],
 
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-			},
-			colors: {
-				link_color: "var(--link-color)",
-				main_text: "var(--main-text-color)",
-				block_border: "var(--block-border-color)",
-				block_background: "var(--block-background-color)",
-			},
-		},
-	},
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                link_color: "var(--link-color)",
+                main_text: "var(--main-text-color)",
+                block_border: "var(--block-border-color)",
+                block_background: "var(--block-background-color)",
+            },
+        },
+    },
 
-	plugins: [forms],
+    plugins: [forms],
 };
